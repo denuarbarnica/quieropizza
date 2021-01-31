1c27404acc928cee5f20118f07c18287a55d91f8
Denuar <Denuar@FABRICIOBARNICA>
2021-01-31 16:21:16 -0600

    Agregamos una lista de Producto

--- QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
+++ QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
@@ -12,11 +12,30 @@ namespace QuieroPizza.Web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto = new ProductoModel();
-            producto.Id = 1;
-            producto.Descripcion = "Pizza 6 Quesos";
+            var producto1 = new ProductoModel();
+            producto1.Id = 1;
+            producto1.Descripcion = "Pizza 6 Quesos";
 
-            return View(producto);
+            var producto2 = new ProductoModel();
+            producto2.Id = 2;
+            producto2.Descripcion = "Pizza 4 Estaciones";
+
+            var producto3 = new ProductoModel();
+            producto3.Id = 3;
+            producto3.Descripcion = "Pizza Jamon y Queso";
+
+            var producto4 = new ProductoModel();
+            producto4.Id = 4;
+            producto4.Descripcion = "Pizza Artesanal";
+
+            var listadeProductos = new List<ProductoModel>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+            listadeProductos.Add(producto4);
+
+
+            return View(listadeProductos);
         }
     }
 }
